bba9ef2cdaf323bbbdb89bf196391b0cf0485293
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-04-28 21:07:06 +0530

    Add files via upload

---
+++ ISSC 75/TwoSum.cpp
@@ -0,0 +1,7 @@
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) 
+        {
+            //Logic, LLO: if the two numbers give an expected result, mark their index and return that o/p. Two parts: Traversal and checking  
+        }
+    };
