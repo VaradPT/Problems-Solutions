7fe60560bb579e47c346ee9963e98ca3e884bc35
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-04-29 22:41:28 +0530

    Add files via upload

---
+++ ISSC 75/Heights.cpp
@@ -0,0 +1,7 @@
+class Solution {
+    public:
+        int heightChecker(vector<int>& heights) 
+        {
+            //Sort the given array of heights and compare sorted[i] with original[i]    
+        }
+    };
