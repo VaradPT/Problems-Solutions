421d401901828f36403caecaee4319e51978924f
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-05-08 22:42:58 +0530

    Update Heights.cpp

--- ISSC 75/Heights.cpp
+++ ISSC 75/Heights.cpp
@@ -1,7 +1,8 @@
-class Solution {
+class Solution
+{
     public:
         int heightChecker(vector<int>& heights) 
         {
             //Sort the given array of heights and compare sorted[i] with original[i]    
         }
-    };
+};
