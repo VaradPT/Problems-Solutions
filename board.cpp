0ce6659ffb5cddca703a82c7b0b3b555c67d7876
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-05-25 23:31:15 +0530

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -1,20 +1,24 @@
 #include "board.h"
 #include <iostream>
 
-Board::Board() {
+Board::Board()
+{
     for (int i = 0; i < SIZE; i++)
         for (int j = 0; j < SIZE; j++)
             board[i][j] = '-';  // Initialize board with empty spaces
 }
 
-void Board::display() {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+void Board::display() 
+{
+    for (int i = 0; i < SIZE; i++)
+        {
+        for (int j = 0; j < SIZE; j++) 
+            {
             if (board[i][j] == 'Q')
                 std::cout << "\033[47m " << board[i][j] << " \033[0m"; // Highlight queens
             else
                 std::cout << "\033[40m " << board[i][j] << " \033[0m"; // Dark background
-        }
+            }
         std::cout << std::endl;
     }
 }
