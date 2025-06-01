536d3887185d79e43c40611fdbc2fd3b41e01bdb
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-06-01 22:38:54 +0530

    Update Heights.cpp

--- ISSC 75/Heights.cpp
+++ ISSC 75/Heights.cpp
@@ -3,6 +3,7 @@ class Solution
     public:
         int heightChecker(vector<int>& heights) 
         {
-            //Sort the given array of heights and compare sorted[i] with original[i]    
+            //Sort the given array of heights and compare sorted[i] with original[i]   
+            
         }
 };
