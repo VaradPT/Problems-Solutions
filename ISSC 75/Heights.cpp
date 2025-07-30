a9e47905ad221de5cd7d8b48674278e81e3a1adc
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-07-30 23:53:18 +0530

    Update Heights.cpp

--- ISSC 75/Heights.cpp
+++ ISSC 75/Heights.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int heightChecker(vector<int>& heights) {
         vector<int> expected = heights;              //Declaring array 'expected' and copying contents of 'heights'  
-
         
         sort(expected.begin(),expected.end());       
         /* Inbuilt cpp Introspective sort (Quicksort + heap sort + insertion sort)
